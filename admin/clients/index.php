bc58a043d8918acd286a5437f75229ef79646c9f
Khaled Zeid <i5aledzeid@gmail.com>
2023-08-05 02:09:29 +0300

    Add files via upload

---
+++ admin/clients/index.php
@@ -0,0 +1,116 @@
+<?php if($_settings->chk_flashdata('success')): ?>
+<script>
+	alert_toast("<?php echo $_settings->flashdata('success') ?>",'success')
+</script>
+<?php endif;?>
+<div class="card card-outline card-primary">
+	<div class="card-header">
+		<h3 class="card-title">List of Products</h3>
+		<div class="card-tools">
+			<a href="?page=clients/manage_client" class="btn btn-flat btn-primary"><span class="fas fa-plus"></span>  Create New Clients</a>
+		</div>
+	</div>
+	<div class="card-body">
+		<div class="container-fluid">
+        <div class="container-fluid">
+			<table class="table table-hover table-strip">
+				<colgroup>
+					<col width="5%">
+					<col width="20%">
+					<col width="20%">
+					<col width="30%">
+					<col width="10%">
+					<col width="15%">
+				</colgroup>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Name</th>
+						<th>Default Delivery Address</th>
+						<th>Contact</th>
+						<th>Email</th>
+						<th>Email</th>
+						<th>Gender</th>
+						<th>Date Created</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php 
+					$i = 1;
+						$qry = $conn->query("SELECT * from `clients`");
+						while($row = $qry->fetch_assoc()):
+							foreach($row as $k=> $v){
+								$row[$k] = trim(stripslashes($v));
+							}
+                            $row['specs'] = strip_tags(stripslashes(html_entity_decode($row['gender'])));
+					?>
+						<tr>
+							<td class="text-center"><?php echo $i++; ?></td>
+							<td><?php echo $row['firstname'] .' '. $row['lastname'] ?></td>
+							<td><?php echo $row['date_created'] ?></td>
+							<td ><p class="m-0 truncate"><?php echo $row['contact'] ?></p></td>
+							<!--<td class="text-center">
+                                <?php if($row['status'] == 1): ?>
+                                    <span class="badge badge-success">Active</span>
+                                <?php else: ?>
+                                    <span class="badge badge-danger">Inactive</span>
+                                <?php endif; ?>
+                            </td>-->
+							<td><?php echo $row['email'] ?></td>
+							<td><?php echo $row['default_delivery_address'] ?></td>
+							<td><?php echo $row['gender'] ?></td>
+							<td align="center">
+								 <button type="button" class="btn btn-flat btn-default btn-sm dropdown-toggle dropdown-icon" data-toggle="dropdown">
+				                  		Action
+				                    <span class="sr-only">Toggle Dropdown</span>
+				                  </button>
+				                  <div class="dropdown-menu" role="menu">
+				                    <a class="dropdown-item" href="?page=product/manage_product&id=<?php echo $row['id'] ?>"><span class="fa fa-edit text-primary"></span> Edit</a>
+				                    <div class="dropdown-divider"></div>
+				                    <a class="dropdown-item delete_data" href="javascript:void(0)" data-id="<?php echo $row['id'] ?>"><span class="fa fa-trash text-danger"></span> Delete</a>
+				                  </div>
+							</td>
+						</tr>
+					<?php endwhile; ?>
+				</tbody>
+			</table>
+		</div>
+		</div>
+	</div>
+</div>
+<script>
+	$(document).ready(function(){
+		$('.delete_data').click(function(){
+			_conf("Are you sure to delete this product permanently?","delete_product",[$(this).attr('data-id')])
+		})
+		$('.table').dataTable({
+			columnDefs: [
+					{ orderable: false, targets: [5] }
+			],
+			order:[0,'asc']
+		});
+		$('.dataTable td,.dataTable th').addClass('py-1 px-2 align-middle')
+	})
+	function delete_product($id){
+		start_loader();
+		$.ajax({
+			url:_base_url_+"classes/Master.php?f=delete_product",
+			method:"POST",
+			data:{id: $id},
+			dataType:"json",
+			error:err=>{
+				console.log(err)
+				alert_toast("An error occured.",'error');
+				end_loader();
+			},
+			success:function(resp){
+				if(typeof resp== 'object' && resp.status == 'success'){
+					location.reload();
+				}else{
+					alert_toast("An error occured.",'error');
+					end_loader();
+				}
+			}
+		})
+	}
+</script>
